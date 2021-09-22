9824b98131ba6bff26ecf239fa1cdd8f99293706
Jordy Lima <jordylima51@gmail.com>
2021-09-22 00:41:33 -0300

    Inital Commit - Project Created

---
+++ src/main/java/com/desafio/java/web/desafioJavaWeb/DesafioJavaWebApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.java.web.desafioJavaWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioJavaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioJavaWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafio/java/web/desafioJavaWeb/DesafioJavaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.java.web.desafioJavaWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioJavaWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
