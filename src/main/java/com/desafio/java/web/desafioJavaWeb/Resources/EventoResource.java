16bc78725cedc6ab6546c2ccce1e089014d13f6b
Jordy Lima <jordylima51@gmail.com>
2021-09-22 22:58:42 -0300

    implementação GetById Usuario e Evento

--- src/main/java/com/desafio/java/web/desafioJavaWeb/Resources/EventoResource.java
+++ src/main/java/com/desafio/java/web/desafioJavaWeb/Resources/EventoResource.java
@@ -1,13 +1,16 @@
 package com.desafio.java.web.desafioJavaWeb.Resources;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.desafio.java.web.desafioJavaWeb.Entities.Evento;
@@ -21,8 +24,13 @@ public class EventoResource {
 	private EventoService eService;
 
 	@GetMapping
-	public ResponseEntity<List<Evento>> findEventoById() {
+	public ResponseEntity<List<Evento>> findAllEvento() {
 		return ResponseEntity.ok().body(eService.findAll());
+	}
+	
+	@GetMapping(path = "/{id}")
+	public ResponseEntity<Optional<Evento>> findEventoById(@PathVariable(value="id") int id) {
+		return ResponseEntity.ok().body(eService.findById((long) id));
 	}
 
 	@PostMapping
--- src/main/java/com/desafio/java/web/desafioJavaWeb/Resources/UsuarioResource.java
+++ src/main/java/com/desafio/java/web/desafioJavaWeb/Resources/UsuarioResource.java
@@ -1,13 +1,16 @@
 package com.desafio.java.web.desafioJavaWeb.Resources;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.desafio.java.web.desafioJavaWeb.Entities.Usuario;
@@ -21,8 +24,13 @@ public class UsuarioResource {
 	private UsuarioService uService;
 
 	@GetMapping
-	public ResponseEntity<List<Usuario>> findUsuarioById() {
+	public ResponseEntity<List<Usuario>> findAllUsuario() {
 		return ResponseEntity.ok().body(uService.findAll());
+	}
+	
+	@GetMapping(path = "/{id}")
+	public ResponseEntity<Optional<Usuario>> findUsuarioById(@PathVariable(value="id") int id) {
+		return ResponseEntity.ok().body(uService.findById((long) id));
 	}
 	
 	@PostMapping
